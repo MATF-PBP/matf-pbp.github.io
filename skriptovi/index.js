b4ae89a4234fed531101ebf821e1bce03e2f64f8
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2020-12-09 21:07:02 +0100

    Mala ispravka u Node skriptu

--- skriptovi/index.js
+++ skriptovi/index.js
@@ -2,7 +2,7 @@ const symbolConverter = require('./symbol-converter');
 const includeSources = require('./include-sources');
 const path = require('./path');
 
-const dirPath = __dirname + '/../poglavlja/';
+const dirPath = __dirname + '/../vezbe/poglavlja/';
 path.iterateOverPathAndExecuteCallback(
   dirPath,
   symbolConverter.translateFromTexEscape
