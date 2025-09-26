11f5b2a0a67654472f9bf1bcbc49584b92281356
RobertDoza <RobertDoza@users.noreply.github.com>
2025-09-26 06:54:35 +0000

    Deploy to GitHub pages

---
+++ skriptovi/path.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+module.exports.iterateOverPathAndExecuteCallback = function (path, callback) {
+  const files = fs.readdirSync(path, { withFileTypes: true });
+  for (const file of files) {
+    const filename = path + '/' + file.name;
+
+    if (filename.endsWith('.markdown')) {
+      let text = fs.readFileSync(filename, { encoding: 'utf8' });
+      text = callback(text);
+      fs.writeFileSync(filename, text);
+    }
+
+    if (file.isDirectory()) {
+      this.iterateOverPathAndExecuteCallback(filename, callback);
+    }
+  }
+};
